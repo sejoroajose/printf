b8fc1028a715c0d64aa526bd8cd6e661ad21bf43
sejoroajose <ajosesejoro@gmail.com>
2023-09-19 15:16:27 +0100

    printf

--- _printf.c
+++ _printf.c
@@ -25,11 +25,6 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			format++;
-			if (*format == '\0')
-			{
-				break;
-			}
 			if (*format == '%')
 			{
 				write(1, format, 1);
@@ -45,14 +40,9 @@ int _printf(const char *format, ...)
 			else if (*format == 's')
 			{
 				char *tsr = va_arg(list_args, char*);
-				int str_len = 0;
 
-				while (tsr[str_len] != '\0')
-				{
-					str_len++;
-				}
 				write(1, tsr, str_len);
-				chr_to_print += str_len;
+				chr_to_print += strlen(tsr);
 			}
 		}
 		format++;
