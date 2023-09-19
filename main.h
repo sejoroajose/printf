aa2ad058f0973fa57fad155a66ae5c8b1e2c8468
sejoroajose <ajosesejoro@gmail.com>
2023-09-20 00:59:43 +0100

    update

--- main.h
+++ main.h
@@ -8,5 +8,6 @@ int _printf(const char *format, ...);
 int print_int(va_list arguments, char *buf, unsigned int ibuf);
 unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
 int print_buf(char *buf, unsigned int nbuf);
+char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
 
 #endif
