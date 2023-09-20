c6f3ca57bc534fc2db591c0e2799335435eeaefb
dedoyin21 <adedirandedoyin@gmail.com>
2023-09-20 03:13:31 +0100

    buffer

--- _buffer.c
+++ _buffer.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_buf - prints buffer
- * @buf: buffer pointer
- * @nbuf: number of bytes to print
+ * print_buf - prints out the buffer
+ *
+ * @buf: buffer arg pointer
+ * @nbuf: number of bytes to print out
  * Return: number of bytes printed.
  */
 int print_buf(char *buf, unsigned int nbuf)
