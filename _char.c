532d37a431a1afd11e44f6e0d4b8ee89d379eb0d
dedoyin21 <adedirandedoyin@gmail.com>
2023-09-20 03:16:25 +0100

    char

--- _char.c
+++ _char.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_chr - writes the character c to stdout
- * @arguments: input char
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
- * Return: On success 1.
+ * print_chr - writes out the char to stdout
+ * 
+ * @arguments: input the char
+ * @buf: buffer arg pointer
+ * @ibuf: index for arg buffer pointer
+ * Return: On success .
  */
 int print_chr(va_list arguments, char *buf, unsigned int ibuf)
 {
