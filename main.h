677395b29ec3ceb467314673c2965fb58eaab7d3
sejoroajose <ajosesejoro@gmail.com>
2023-09-20 02:59:33 +0100

    printf project

--- main.h
+++ main.h
@@ -1,8 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 #include <stdarg.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 /**
  * struct print - struct for printer functions
@@ -12,21 +13,12 @@
  * Description: struct that stores pointers to a
  * printer functions.
  */
-
-
 typedef struct print
 {
 	char *type_arg;
 	int (*f)(va_list, char *, unsigned int);
 } print_t;
 
-
-int _printf(const char *format, ...);
-int print_int(va_list arguments, char *buf, unsigned int ibuf);
-unsigned int handl_buf(char *buf, char c, unsigned int ibuf);
-int print_buf(char *buf, unsigned int nbuf);
-char *fill_binary_array(char *binary, long int int_in, int isneg, int limit);
-int prinntupx(va_list arguments, char *buf, unsigned int ibuf)
 int _printf(const char *format, ...);
 int print_prg(va_list __attribute__((unused)), char *, unsigned int);
 int print_chr(va_list arguments, char *buf, unsigned int ibuf);
