b5ece2dda0261097fe870bd4348797ba046b86d1
dedoyin21 <adedirandedoyin@gmail.com>
2023-09-20 03:06:57 +0100

    address

--- _address.c
+++ _address.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_add - prints the address of an input variable
+ * print_add -the address of an input 
  * @arguments: input address.
- * @buf: buffer pointer.
- * @ibuf: index for buffer pointer
+ * @buf: arg buffer pointer.
+ * @ibuf: index for arg buffer pointer
  *
- * Return: number of chars printed.
+ * Return: number of characters printed.
  */
 int print_add(va_list arguments, char *buf, unsigned int ibuf)
 {
