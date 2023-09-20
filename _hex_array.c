50a80cbbaea4018449bf533d2086a286fccf5ed5
dedoyin21 <adedirandedoyin@gmail.com>
2023-09-20 03:26:25 +0100

    hex_array

--- _hex_array.c
+++ _hex_array.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * fill_hex_array - writes the character c to stdout
+ * fill_hex_array - writes the char to stdout
  *
- * @bnr: array where is stored the binary.
- * @hex: array where is stored the hexadecimal.
- * @isupp: integer that determines if the hexadecimal array is
+ * @bnr: the array where binary is stored.
+ * @hex: the array where hexadecimal is stored.
+ * @isupp: integer that determines the hexadecimal array is
  * in uppercase or lowercase letter.
  * @limit: size of hex
  * Return: binary array.
