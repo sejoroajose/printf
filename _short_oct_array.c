228a854f2225256e6b113d7f7ad9736885295931
sejoroajose <ajosesejoro@gmail.com>
2023-09-20 03:59:14 +0100

    update

--- _short_oct_array.c
+++ _short_oct_array.c
@@ -4,10 +4,9 @@
  * fill_short_oct_array - calculates a short octal number
  *
  * @bnr: array where is stored the binary.
- * @oct: array where is stored the octal.
- *
  * Return: binary array.
  */
+
 char *fill_short_oct_array(char *bnr, char *oct)
 {
 	int op, i, j, ioct, limit;
