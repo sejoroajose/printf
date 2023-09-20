51d701730eaa3a0be49aef795dbfd1496261cc4b
sejoroajose <ajosesejoro@gmail.com>
2023-09-20 03:56:41 +0100

    update

--- _space_int.c
+++ _space_int.c
@@ -3,10 +3,9 @@
 /**
  * prinsint - prints int begining with space
  * @arguments: input string
- * @buf: buffer pointer
- * @ibuf: index for buffer pointer
  * Return: number of chars printed
  */
+
 int prinsint(va_list arguments, char *buf, unsigned int ibuf)
 {
 	int int_input;
